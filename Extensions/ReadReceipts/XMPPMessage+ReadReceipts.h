c7e6141ba409e93ea8edeb4cadee8d486b85880a
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2020-04-05 21:45:29 +0300

    Fixed user type in new method

--- Extensions/ReadReceipts/XMPPMessage+ReadReceipts.h
+++ Extensions/ReadReceipts/XMPPMessage+ReadReceipts.h
@@ -21,7 +21,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, readonly, nullable) XMPPMessage *generateReadReceiptResponse;
 
 // Global read receipt for all messages
-+ (XMPPMessage *)generateReadReceiptForJid:(NSString *)jid;
++ (XMPPMessage *)generateReadReceiptForJid:(XMPPJID *)jid;
 
 - (void)addReadReceiptRequest;
 
